5b77876bae86f8f72043d32cb88d5e525f33c90f
Harry Nguyen <50328363+HarryNguyen99@users.noreply.github.com>
2020-03-22 15:52:25 +0700

    số nguyên tố nhỏ hơn 100

---
+++ src/HienThiSNTNhoHoen100.java
@@ -0,0 +1,25 @@
+public class HienThiSNTNhoHoen100 {
+    public static void main(String[] args) {
+        int number = 0;
+        boolean check = false;
+
+        for (number = 2; number < 100; number++) {
+            if (number < 2) {
+            } else {
+                int i = 2;
+                check = true;
+                while (i <= Math.sqrt(number)) {
+                    if (number % i == 0) {
+                        check = false;
+                        break;
+                    }
+                    i++;
+                }
+            }
+            if (check)
+                System.out.println("số nguyên tố nhỏ hơn 100: " + number);
+        }
+
+
+    }
+}
